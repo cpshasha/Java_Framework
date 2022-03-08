ec3cd21f1f7277588dcca100c3080a897c1dc81e
changsha <997201679@qq.com>
2022-03-08 10:12:02 +0800

    MyBatis-05|06\07\08
    properties，别名，日志

---
+++ 02-MyBatis-07-Log/src/main/java/com/fc/dao/StudentDao.java
@@ -0,0 +1,12 @@
+package com.fc.dao;
+
+import com.fc.entity.Student;
+
+import java.util.List;
+
+public interface StudentDao {
+
+    List<Student> findAll();
+
+    Student findById(Integer id);
+}
